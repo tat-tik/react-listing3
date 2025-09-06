ba1a132fe6bf242736de7838a2d157838d5bcb09
Tanya <an5227952@gmail.com>
2025-09-06 19:54:09 +0300

    добавила ? в itemdata

--- src/components/model.tsx
+++ src/components/model.tsx
@@ -1,13 +1,13 @@
 export type ItemData = {
-    listing_id: number;
-    url: string;
-    MainImage: {
+    listing_id?: number;
+    url?: string;
+    MainImage?: {
         "url_570xN": string;
     };
-    title: string;
-    currency_code: string;
-    price: string;
-    quantity: number;
+    title?: string;
+    currency_code?: string;
+    price?: string;
+    quantity?: number;
     };   
 
 export type ItemError = {
